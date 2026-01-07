25b317e88622e4c06196e74dbf0c7d0b1b21b1dc
LERATO LESHABANE <leshabanellh91@gmail.com>
2026-01-07 15:08:38 +0200

    Add project files.

---
+++ MyFirstMauiApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MyFirstMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            CounterBtn.Text = count == 1
+                ? $"Clicked {count} time"
+                : $"Clicked {count} times";
+
+            ResultLabel.Text = "Button clicked 🎉";
+        }
+    }
+}
